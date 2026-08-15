f651176f5a1438bcac17b34d3b8ff1085909068f
eliza <eliza@local>
2026-08-15 01:31:14 -0500

    fix: use correct supabase.co project domain

--- english-basic/src/environments/environment.ts
+++ english-basic/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://qeopcwsuwntmjmodiwpt.supabase.com',
+  supabaseUrl: 'https://qeopcwsuwntmjmodiwpt.supabase.co',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlb3Bjd3N1d250bWptb2Rpd3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NjIxODEsImV4cCI6MjEwMjMzODE4MX0.RZHjcnkFjpFdiUQqeSUIbBCfLdtdHNB80s-wvOm4seA',
 };
